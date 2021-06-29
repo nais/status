faa6945d07e4fbe6fe8435be1c6a4b4065d39a7c
Johnny Horvi <johnny@horvi.no>
2021-06-29 09:32:36 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type statusResponse struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Team        string `json:"team"`
+	Status      string `json:"status"`
+}
+
+var (
+	clusterName string
+	bindAddr    string
+)
+
+func init() {
+	flag.StringVar(&bindAddr, "bind-address", ":8080", "ip:port where http requests are served")
+	flag.StringVar(&clusterName, "cluster-name", os.Getenv("NAIS_CLUSTER_NAME"), "which cluster we are running in")
+	flag.Parse()
+}
+
+func main() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		status := &statusResponse{
+			Name:        fmt.Sprintf("nais-%s", clusterName),
+			Description: fmt.Sprintf("NAIS status in cluster %s", clusterName),
+			Team:        "aura",
+			Status:      "ok",
+		}
+		if err := json.NewEncoder(w).Encode(status); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(fmt.Fprint(w, "uh oh"))
+		}
+	})
+
+	fmt.Println("running @", bindAddr)
+	go func() {
+		log.Fatal((&http.Server{Addr: bindAddr}).ListenAndServe())
+	}()
+
+	<-interrupt
+
+	fmt.Println("shutting down")
+	fmt.Println((&http.Server{Addr: bindAddr}).Shutdown(context.Background()))
+}
